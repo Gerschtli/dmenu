2304e14ce837070ea0adf8e7f01e9cd04b903beb
Tobias Happ <tobias.happ@gmx.de>
2021-11-07 20:46:42 +0100

    Remove old files

--- create-patch.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-VERSION="${1}"
-FILE="${2:-patch-"${1}".diff}"
-
-git config --local diff.mnemonicprefix true
-git diff --ignore-space-at-eol "${VERSION}" --  . ':!*.diff' ':!.git*' ':!create-patch.sh' > "${FILE}"
